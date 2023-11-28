2dbfd3b6b001ff863dfd02ccf4acd7ed65357cd6
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-29 01:39:20 +0500

    start

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -5,9 +5,9 @@ import { NextResponse } from "next/server";
 export async function PUT(request, { params }) {
   const { id } = params;
   console.log("request", await request);
-  // const { newTitle: title, newDescription: description } = await request.json();
-  // await connectMongoDB();
-  // await Topic.findByIdAndUpdate(id, { title, description });
+  const { newTitle: title, newDescription: description } = await request.json();
+  await connectMongoDB();
+  await Topic.findByIdAndUpdate(id, { title, description });
   return NextResponse.json({ message: "Topic updated" }, { status: 200 });
 }
 
