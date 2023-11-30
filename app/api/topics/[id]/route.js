c9ae9b4048b1853840d5680e075ef4e0f05bf704
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-30 11:33:32 +0500

    auth

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -3,17 +3,36 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
-  const { id } = params;
-  console.log("request", await request);
-  const { newTitle: title, newDescription: description } = await request.json();
-  await connectMongoDB();
-  await Topic.findByIdAndUpdate(id, { title, description });
-  return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+  try {
+    const { id } = params;
+    const { newTitle: title, newDescription: description } =
+      await request.json();
+
+    await connectMongoDB();
+    await Topic.findByIdAndUpdate(id, { title, description });
+
+    return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+  } catch (error) {
+    console.error("Error updating topic:", error);
+    return NextResponse.json(
+      { error: "Failed to update topic" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function GET(request, { params }) {
-  const { id } = params;
-  await connectMongoDB();
-  const topic = await Topic.findOne({ _id: id });
-  return NextResponse.json({ topic }, { status: 200 });
+  try {
+    const { id } = params;
+    await connectMongoDB();
+    const topic = await Topic.findOne({ _id: id });
+
+    return NextResponse.json({ topic }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching topic:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch topic" },
+      { status: 500 }
+    );
+  }
 }
